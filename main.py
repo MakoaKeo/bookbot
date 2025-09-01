1483713748300bf532f0110f7aef051cf882beb2
MakoaKeo <strykerd@gmail.com>
2025-09-01 14:44:14 -0500

    updated main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def get_book_text():
-    with open("./books/frankenstein.txt") as f:
-        file_contents = f.read()  
-    return file_contents
-
 def main():
-    get_book_text()
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
 
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 main()
