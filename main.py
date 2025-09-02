af02ebf43a5e5af30449d4d70ab4a0e8c5324231
MakoaKeo <strykerd@gmail.com>
2025-09-02 16:50:25 -0500

    updated main.py

--- main.py
+++ main.py
@@ -3,17 +3,7 @@ def main():
     text = get_book_text(book_path)
     print(text)
 
-def find_word_count(path):
-    book_text = []
-    num_words = []
-    
-    for word in story:
-        book_text.append(word)
-    num_words.append(len(book_text))
-
-    print(f"{num_words} words found in the document")
-
-
-
-
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 main()
