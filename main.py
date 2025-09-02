a81be33f9166c955ee2a7ef18f7553c4f3a4188e
MakoaKeo <strykerd@gmail.com>
2025-09-02 18:13:17 -0500

    CH2 L6 complete

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from stats import get_num_words
+from stats import get_num_words, get_character_count
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
+    letter_count = get_character_count(text)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    print(f"{num_words} found in the document")
+    print(letter_count)
+
 
 
 def get_book_text(path):
--- stats.py
+++ stats.py
@@ -2,3 +2,16 @@ def get_num_words(body):
     num_words = len(body.split())
 
     return num_words
+
+
+def get_character_count(body):
+    character_count = {}
+    words = body.split()
+    for word in words:
+        lc_let = word.lower()
+        for letter in lc_let:
+            if letter not in character_count:
+                character_count[letter] = 1
+            else:
+                character_count[letter] += 1
+    return character_count
