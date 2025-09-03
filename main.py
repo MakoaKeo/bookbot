4af8c6e84438832af4d25c62900d9416e047cc21
MakoaKeo <strykerd@gmail.com>
2025-09-03 17:10:44 -0500

    CH3, L2 complete

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import sys
 from stats import get_num_words, get_character_count, get_sorted_list
 
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     letter_count = get_character_count(text)
     num_words = get_num_words(text)
